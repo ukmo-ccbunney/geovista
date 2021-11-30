594032f2b483f49487d86618bb9740d9f9d44193
Chris Bunney <chris@Chriss-MacBook-Air.local>
2021-11-30 07:49:19 +0000

    Added ustructured FE grid example from WW3 output - doesn't work at the moment though...

---
+++ examples/example_from_unstructured__ww3.py
@@ -0,0 +1,61 @@
+import netCDF4 as nc
+import geovista as gv
+
+d = nc.Dataset('./ww3.201809_hs.nc')
+lon = d.variables['longitude'][:]
+lat = d.variables['latitude'][:]
+tri = d.variables['tri'][:]
+
+hs = d.variables['hs']
+
+xverts = lon[tri-1]
+yverts = lat[tri-1]
+
+# I have to "clean" mesh to remove duplicate points,
+# otherwise the number of points does not match the
+# size of the data arrays. I can't seem to add the
+# data array in the calls though regarsless of whether
+# clean is True or False...
+mesh=gv.Transform.from_unstructured(xverts, yverts, xverts.shape, name='tri', clean=True)
+
+# Setting point data as scalars here works...
+# but the values are clearly not in the right place...
+mesh.point_data.set_scalars(hs[7,:])
+
+plotter = gv.GeoBackgroundPlotter()
+plotter.add_mesh(mesh, show_edges=True)
+
+plotter.show()
+
+
+## show points in matplotlib how it should look
+import matplotlib.pyplot as plt
+plt.scatter(lon,lat,c=hs[7,:],s=10)
+plt.show()
+
+#####
+## For reference: NC dump of input file (trimmer to pertinent bits):
+
+netcdf ww3.201809_hs {
+dimensions:
+	node = 16160 ;
+	element = 30559 ;
+	time = UNLIMITED ; // (17 currently)
+	noel = 3 ;
+variables:
+	float longitude(node) ;
+		longitude:units = "degree_east" ;
+		longitude:standard_name = "longitude" ;
+	float latitude(node) ;
+		latitude:units = "degree_north" ;
+		latitude:standard_name = "latitude" ;
+	double time(time) ;
+		time:standard_name = "time" ;
+		time:units = "days since 1990-01-01T00:00:00" ;
+		time:calendar = "standard" ;
+	int tri(element, noel) ;
+
+	float hs(time, node) ;
+		hs:standard_name = "sea_surface_wave_significant_height" ;
+
+}
